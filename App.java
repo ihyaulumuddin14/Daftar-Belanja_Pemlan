ddb924c0f36a7a21f00d142bb9bc240860dbd5d3
Ihya' Ulumuddin <pebehakun45@gmail.com>
2025-02-28 09:15:18 +0700

    hapus file .class

--- App.java
+++ App.java
@@ -5,7 +5,26 @@ public class App {
         Scanner in = new Scanner(System.in);
 
         while (true) { 
-            
+            System.out.println("Selamat Datang di Aplikasi Belanja Online\n");
+            System.out.println("Menu:");
+            System.out.println("1. Tambah Item");
+            System.out.println("2. Tampilkan Semua Item");
+            System.out.println("3. Keluar");
+            System.out.print("Pilihan: ");
+            int pilihan = in.nextInt();
+
+            if (pilihan == 1) {
+                System.out.print("Masukkan Nama Item: ");
+                String nama = in.next();
+                System.out.print("Masukkan Jumlah Item: ");
+                int jumlah = in.nextInt();
+                Item item = new Item(nama, jumlah);
+                daftarBelanja.tambahItem(item);
+            } else if (pilihan == 2) {
+                daftarBelanja.tampilSemuaItem();    
+            } else if (pilihan == 3) {
+                break;
+            }
         }
     }
 }
